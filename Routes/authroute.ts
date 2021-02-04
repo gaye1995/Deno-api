32a43c3c238ff47840de1f2f4686de62897041b4
gaye <gayegayemboup@gmail.com>
2021-02-04 20:47:51 +0100

    merge

--- Routes/authroute.ts
+++ Routes/authroute.ts
@@ -2,6 +2,7 @@ import { Router } from 'https://deno.land/x/abc@v1.2.4/router.ts';
 import { Context } from "https://deno.land/x/abc@v1.2.4/context.ts";
 import { HttpMethod } from "https://deno.land/x/abc@v1.2.4/constants.ts";
 import { UsersControllers } from '../controllers/UsersControllers.ts';
+const router = new Router();
 
 router.add(HttpMethod.Post, '/register', UsersControllers.register);
 router.add(HttpMethod.Post, '/login', UsersControllers.login);
@@ -12,7 +13,6 @@ router.add(HttpMethod.Post, '/user/child',UsersControllers.userchild)
 /*import { Router } from "https://deno.land/x/oak/mod.ts";
 import {UsersControllers} from '../controllers/UsersControllers.ts';
 
-const router = new Router();
 
 router.post("/login",UsersControllers.login);
 */
