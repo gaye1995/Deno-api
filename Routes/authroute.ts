68e3e784bc6b172ef513354a71be0f791ef8e233
gaye <gayegayemboup@gmail.com>
2021-02-10 00:42:28 +0100

    initialisation de la route /song

--- Routes/authroute.ts
+++ Routes/authroute.ts
@@ -4,6 +4,7 @@ import { HttpMethod } from "https://deno.land/x/abc@v1.2.4/constants.ts";
 import { UsersControllers } from '../controllers/UsersControllers.ts';
 import {StripeControllers} from '../controllers/StripeControllers.ts';
 import { CartControllers } from '../controllers/CartControllers.ts';
+import {SongControllers} from '../controllers/SongControllers.ts';
 
 const router = new Router();
 
@@ -13,11 +14,6 @@ router.add(HttpMethod.Put, '/subscription',StripeControllers.subsstripe)
 router.add(HttpMethod.Post, '/user/child',UsersControllers.userchild)
 router.add(HttpMethod.Post, '/user/cart',CartControllers.usercart)
 router.add(HttpMethod.Delete, '/user', UsersControllers.deleteuser);
+router.add(HttpMethod.Get, '/user/{id}', SongControllers.song);
 
-
-
-/*import { Router } from "https://deno.land/x/oak/mod.ts";
-import {UsersControllers} from '../controllers/UsersControllers.ts';
-router.post("/login",UsersControllers.login);
-*/
   export { router }
---
+++ controllers/SongControllers.ts
@@ -0,0 +1,8 @@
+import { play } from "https://deno.land/x/audio@0.1.0/mod.ts";
+import { HandlerFunc } from 'https://deno.land/x/abc@v1.2.4/types.ts';
+import { Context } from 'https://deno.land/x/abc@v1.2.4/context.ts';
+export class SongControllers {
+
+    static song: HandlerFunc = async(c: Context) => {
+    }
+}
